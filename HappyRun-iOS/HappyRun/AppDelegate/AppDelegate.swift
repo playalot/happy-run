6e4040e51d1c3220dc745bab4417275be0c12b45
TBXark <tbxark@outlook.com>
2017-10-13 16:23:51 +0800

    iOS project init

---
+++ HappyRun-iOS/HappyRun/AppDelegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  HappyRun
+//
+//  Created by Tbxark on 13/10/2017.
+//  Copyright © 2017 Tbxark. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        window?.rootViewController = GameViewController()
+        window?.backgroundColor = UIColor.black
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ HappyRun-iOS/HappyRun/Modules/Game/GameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GameViewController.swift
+//  HappyRun
+//
+//  Created by Tbxark on 13/10/2017.
+//  Copyright © 2017 Tbxark. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+}
