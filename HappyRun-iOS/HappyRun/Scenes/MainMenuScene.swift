19ee6a1fa087ce39453114aaf438149ec7ea7eb1
Guan Guan <guanguan1114@gmail.com>
2017-10-13 18:38:54 +0800

    Upload Art Resouces

---
+++ HappyRun-iOS/HappyRun/Scenes/MainMenuScene.swift
@@ -0,0 +1,32 @@
+//
+//  MainMenuScene.swift
+//  HappyRun
+//
+//  Created by Guan Guan on 10/13/17.
+//  Copyright © 2017 Tbxark. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "MainMenu")
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(background)
+    }
+    
+    func sceneTapped() {
+//        let myScene = GameScene(size: size)
+//        myScene.scaleMode = scaleMode
+//        let reveal = SKTransition.doorway(withDuration: 1.5)
+//        view?.presentScene(myScene, transition: reveal)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+        sceneTapped()
+    }
+    
+}
